77aa648bb81601fcd736e27d080012fa088f1684
Stephen <stephenleonardo18@gmail.com>
2021-05-25 19:27:19 +0700

    [IMPLEMENTATION] Creating API method getTranslation

---
+++ api/capstone/api/serializers.py
@@ -0,0 +1,6 @@
+from rest_framework import serializers
+
+
+class AudioSerializer(serializers.HyperlinkModelSerializer):
+    filename = serializers.CharField(max_length=None, allow_null=True)
+    translation = serializers.CharField(max_length=None, allow_null=True)
