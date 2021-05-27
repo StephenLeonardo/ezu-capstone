7e0a08d5a49b63b814d2cfa14373dfad47af861f
Stephen <stephenleonardo18@gmail.com>
2021-05-27 13:51:34 +0700

    [IMPLEMENTATION] adding API

---
+++ api/capstone/api/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from rest_framework import routers
+from .views import getTranslation
+
+app_name = 'api'
+
+urlpatterns = [
+    path('get-translation/', getTranslation.as_view(), name='getTranslation')
+]
