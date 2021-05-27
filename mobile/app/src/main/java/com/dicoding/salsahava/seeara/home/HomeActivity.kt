ec422647efcc4762f21afe9b0bd81cc8b4233992
salsahava <1shavaqabitta@gmail.com>
2021-05-27 07:58:15 +0700

    [REFACTOR] Rename MainActivity to HomeActivity

--- mobile/app/src/main/java/com/dicoding/salsahava/seeara/home/HomeActivity.kt
+++ mobile/app/src/main/java/com/dicoding/salsahava/seeara/home/HomeActivity.kt
@@ -1,9 +1,10 @@
-package com.dicoding.salsahava.seeara
+package com.dicoding.salsahava.seeara.home
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.dicoding.salsahava.seeara.R
 
-class MainActivity : AppCompatActivity() {
+class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
