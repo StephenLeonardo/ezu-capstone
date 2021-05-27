94a6e916b7239683c125713e67d171ddbcedda71
salsahava <1shavaqabitta@gmail.com>
2021-05-27 13:55:15 +0700

    [TRYOUT] Recording tryout, can record one file only

--- mobile/app/src/main/java/com/dicoding/salsahava/seeara/home/HomeActivity.kt
+++ mobile/app/src/main/java/com/dicoding/salsahava/seeara/home/HomeActivity.kt
@@ -3,10 +3,16 @@ package com.dicoding.salsahava.seeara.home
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.dicoding.salsahava.seeara.R
+import com.dicoding.salsahava.seeara.record.RecordFragment
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.fl_home, RecordFragment())
+            commit()
+        }
     }
 }
