2f74fd33e9890d6df676b1332fac857cb4d5edbd
Stephen <stephenleonardo18@gmail.com>
2021-05-28 20:52:19 +0700

    [REFACTOR] trying some stuff

--- api/capstone/api/views.py
+++ api/capstone/api/views.py
@@ -54,52 +54,45 @@ def getTranslation(request):
     filename = request.query_params.get('filename', None)
 
     # Url untuk cloud storage
-    dirname = 'https://storage.googleapis.com/ezu-storage/'
+    dirname = 'C:/Users/Steph/#Binus/#Bangkit/#Project/ezu-capstone/model/'
+
+    audio = audio_files_to_numpy(dirname, [filename], sample_rate,
+                             frame_length, hop_length_frame, min_duration)
+
+    n_fft = 255
+    hop_length_fft = 63
+    dim_square_spec = int(n_fft / 2) + 1
+
+    m_amp_db_audio,  m_pha_audio = numpy_audio_to_matrix_spectrogram(
+    audio, dim_square_spec, n_fft, hop_length_fft)
 
 
-    full_name = dirname + filename
 
-    #
-    X, sample_rate = librosa.load(full_name)
 
-    #
-    # audio = audio_files_to_numpy(dirname, [filename], sample_rate,
-    #                          frame_length, hop_length_frame, min_duration)
-    #
-    # n_fft = 255
-    # hop_length_fft = 63
-    # dim_square_spec = int(n_fft / 2) + 1
-    #
-    # m_amp_db_audio,  m_pha_audio = numpy_audio_to_matrix_spectrogram(
-    # audio, dim_square_spec, n_fft, hop_length_fft)
-    #
-    #
-    #
-    #
-    # #global scaling to have distribution -1/1
-    # X_in = scaled_in(m_amp_db_audio)
-    # #Reshape for prediction
-    # X_in = X_in.reshape(X_in.shape[0],X_in.shape[1],X_in.shape[2],1)
-    # #Prediction using loaded network
-    # X_pred = loaded_model.predict(X_in)
-    # #Rescale back the noise model
-    # inv_sca_X_pred = inv_scaled_ou(X_pred)
-    # #Remove noise model from noisy speech
-    # X_denoise = m_amp_db_audio - inv_sca_X_pred[:,:,:,0]
-    # #Reconstruct audio from denoised spectrogram and phase
-    # audio_denoise_recons = matrix_spectrogram_to_numpy_audio(X_denoise, m_pha_audio, frame_length, hop_length_fft)
-    #
-    #
-    #
-    #
-    # output_file = (audio_denoise_recons.flatten() * 100)
-    # export_filename = 'test.wav'
-    # # url untuk export audio yg udah dijernihin ke cloud storage
-    # export_path = 'C:/Users/Steph/#Binus/#Bangkit/#Project/ezu-capstone/api/api/' + export_filename
-    # sf.write(export_path, output_file, sample_rate)
-    #
-    # callGoogleAPI()
-    #
+    #global scaling to have distribution -1/1
+    X_in = scaled_in(m_amp_db_audio)
+    #Reshape for prediction
+    X_in = X_in.reshape(X_in.shape[0],X_in.shape[1],X_in.shape[2],1)
+    #Prediction using loaded network
+    X_pred = loaded_model.predict(X_in)
+    #Rescale back the noise model
+    inv_sca_X_pred = inv_scaled_ou(X_pred)
+    #Remove noise model from noisy speech
+    X_denoise = m_amp_db_audio - inv_sca_X_pred[:,:,:,0]
+    #Reconstruct audio from denoised spectrogram and phase
+    audio_denoise_recons = matrix_spectrogram_to_numpy_audio(X_denoise, m_pha_audio, frame_length, hop_length_fft)
+
+
+
+
+    output_file = (audio_denoise_recons.flatten() * 100)
+    export_filename = 'test.wav'
+    # url untuk export audio yg udah dijernihin ke cloud storage
+    export_path = 'C:/Users/Steph/#Binus/#Bangkit/#Project/ezu-capstone/api/api/' + export_filename
+    sf.write(export_path, output_file, sample_rate)
+
+    callGoogleAPI()
+
     return Response({'status': 'Success!'})
 
     # export_audio_file(export_path, output_file, sample_rate)
