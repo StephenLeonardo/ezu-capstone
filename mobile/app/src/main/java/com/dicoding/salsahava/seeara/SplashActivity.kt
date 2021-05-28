0963a1bee54be67e314ce959b6f22c417483b336
ushisui45 <58464856+ushisui45@users.noreply.github.com>
2021-05-28 21:57:24 +0700

    add splash

---
+++ mobile/app/src/main/java/com/dicoding/salsahava/seeara/SplashActivity.kt
@@ -0,0 +1,26 @@
+package com.dicoding.salsahava.seeara
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.view.WindowManager
+import com.dicoding.salsahava.seeara.home.HomeActivity
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
+            WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS
+        )
+
+        Handler(mainLooper).postDelayed({
+            val intent = Intent(this, HomeActivity::class.java)
+            startActivity(intent)
+            finish()
+        },2500)
+    }
+}
